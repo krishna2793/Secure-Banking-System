7862a96d0a4a2ee012cf2dced43ce3722ef2a17c
anup <anupshamanur2109@gmail.com>
2020-03-19 13:10:52 -0700

    Integrated admin front end

--- src/main/java/edu/asu/sbs/controllers/AdminController.java
+++ src/main/java/edu/asu/sbs/controllers/AdminController.java
@@ -1,19 +1,20 @@
 package edu.asu.sbs.controllers;
 
-import com.google.gson.Gson;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.jknack.handlebars.Template;
 import edu.asu.sbs.config.RequestType;
 import edu.asu.sbs.config.UserType;
 import edu.asu.sbs.errors.Exceptions;
 import edu.asu.sbs.errors.UnauthorizedAccessExcpetion;
+import edu.asu.sbs.loader.HandlebarsTemplateLoader;
 import edu.asu.sbs.models.Request;
 import edu.asu.sbs.models.User;
 import edu.asu.sbs.services.RequestService;
 import edu.asu.sbs.services.UserService;
 import edu.asu.sbs.services.dto.UserDTO;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.configurationprocessor.json.JSONArray;
 import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +26,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Optional;
 
 @Slf4j
@@ -35,75 +40,99 @@ public class AdminController{
 
     private final UserService userService;
     private final RequestService requestService;
+    private final HandlebarsTemplateLoader handlebarsTemplateLoader;
 
-    public AdminController(UserService userService, RequestService requestService) {
+    ObjectMapper mapper = new ObjectMapper();
+
+    public AdminController(UserService userService, RequestService requestService, HandlebarsTemplateLoader handlebarsTemplateLoader) {
         this.userService = userService;
         this.requestService = requestService;
+        this.handlebarsTemplateLoader = handlebarsTemplateLoader;
     }
 
     @GetMapping("/employee/details")
     @ResponseBody
-    public JSONObject currentUserDetails() throws UnauthorizedAccessExcpetion, JSONException {
+    public String currentUserDetails() throws UnauthorizedAccessExcpetion, JSONException, IOException {
 
         User user = userService.getCurrentUser();
-
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        mapper.setDateFormat(df);
         if (user == null) {
             log.info("GET request: Unauthorized request for admin user detail");
             throw new UnauthorizedAccessExcpetion("401", "Unauthorized Access !");
         }
 
+        JsonNode result = mapper.valueToTree(user);
+        Template template = handlebarsTemplateLoader.getTemplate("profileAdmin");
         log.info("GET request: Admin user detail");
-        return new JSONObject(new Gson().toJson(user));
+        return template.apply(handlebarsTemplateLoader.getContext(result));
+    }
+
+    @GetMapping("/employee/add")
+    public String getLoginTemplate() throws IOException {
+        Template template = handlebarsTemplateLoader.getTemplate("adminAddNewEmployee");
+        return template.apply("");
     }
 
     @PostMapping("/employee/add")
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public void signupSubmit(UserDTO newUserRequest, String password, String userType) throws Exceptions {
+    public void signupSubmit(UserDTO newUserRequest, String password, String userType, HttpServletResponse response) throws Exceptions, IOException {
         userService.registerUser(newUserRequest, password, userType);
         log.info("POST request: Admin new user request");
+        response.sendRedirect("/allEmployees");
     }
 
     @GetMapping("/allEmployees")
-    public JSONArray getUsers() throws Exceptions, JSONException {
-        return new JSONArray(new Gson().toJson(userService.getAllEmployees()));
+    public String getUsers() throws Exceptions, JSONException, IOException {
+        ArrayList<User> allEmployees = (ArrayList<User>) userService.getAllEmployees();
+        HashMap<String, ArrayList<User>> resultMap= new HashMap<>();
+        resultMap.put("result", allEmployees);
+        JsonNode result = mapper.valueToTree(resultMap);
+        Template template = handlebarsTemplateLoader.getTemplate("adminEmployeeAccess");
+        return template.apply(handlebarsTemplateLoader.getContext(result));
     }
 
-    @PostMapping("/delete/{id}")
-    public void deleteUser(@PathVariable Long id) throws Exceptions {
+    @GetMapping("/delete/{id}")
+    public void deleteUser(@PathVariable Long id, HttpServletResponse response) throws Exceptions, IOException {
         Optional<User> current = userService.getUserByIdAndActive(id);
         if (current == null) {
             throw new Exceptions("404", " ");
         }
-        if (!current.get().getUserType().equals("EMPLOYEE_ROLE1")) {
-            log.warn("GET request: Admin unauthrorised request access");
+        if (!(current.get().getUserType().equals("EMPLOYEE_ROLE1") || current.get().getUserType().equals("EMPLOYEE_ROLE2"))) {
+            log.warn("GET request: Admin unauthorised request access");
             throw new Exceptions("401", "Unauthorized request !!");
         }
 
         userService.deleteUser(id);
         log.info("POST request: Employee New modification request");
+        response.sendRedirect("../allEmployees");
     }
 
-
     @GetMapping("/viewEmployee/{id}")
-    public JSONObject getUserDetail(@PathVariable Long id) throws Exceptions, JSONException {
+    public String getUserDetail(@PathVariable Long id) throws Exceptions, JSONException, IOException {
         Optional<User> user = userService.getUserByIdAndActive(id);
+
         if (user == null) {
             throw new Exceptions("404", " ");
         }
-        if (!user.get().getUserType().equals("EMPLOYEE_ROLE1")) {
+        if (!(user.get().getUserType().equals("EMPLOYEE_ROLE1") || user.get().getUserType().equals("EMPLOYEE_ROLE2"))) {
             log.warn("GET request: Unauthorized request for external user");
             throw new Exceptions("409", " ");
         }
 
-        log.info("GET request: Internal user details by id");
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("user", user);
-        return jsonObject;
+        JsonNode result = mapper.valueToTree(user.get());
+        Template template = handlebarsTemplateLoader.getTemplate("adminViewEmployee");
+        return template.apply(handlebarsTemplateLoader.getContext(result));
     }
 
     @GetMapping("/requests")
-    public JSONArray getAllUserRequest() throws JSONException {
-        return new JSONArray(new Gson().toJson(requestService.getAllRequests()));
+    public String getAllUserRequest() throws JSONException, IOException {
+        ArrayList<Request> allRequests = (ArrayList<Request>) requestService.getAllRequests();
+        HashMap<String, ArrayList<Request>> resultMap= new HashMap<>();
+        resultMap.put("result", allRequests);
+        JsonNode result = mapper.valueToTree(resultMap);
+        Template template = handlebarsTemplateLoader.getTemplate("adminHome");
+        return template.apply(handlebarsTemplateLoader.getContext(result));
     }
 
     @PutMapping("/requests/approve/{id}")
